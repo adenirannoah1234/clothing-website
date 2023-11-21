30e86e59031aaacee89a07c3aa51504263476bc0
adenirannoah1234 <adenirannoah9@gmail.com>
2023-11-21 17:37:33 +0100

    initial changes

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,19 @@
+/*import Directory from '../components/directory/directory.component';*/
+
+const Category = ({ category }) => {
+  //   const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        // style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        {/* <h2>{title}</h2> */}
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
